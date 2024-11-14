fc01ff2a107fe974b0a3c4ec396dc66a5c7e818a
Franklin Addo <B00159369@mytudublin.ie>
2024-11-14 12:19:04 +0000

    Obstacles does not spawn once the game over messages appears and the obstacles destroys after they leave the scene

--- Assets/Scripts/MoveBack.cs
+++ Assets/Scripts/MoveBack.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class MoveBack : MonoBehaviour
 {
     private float speed = 30.0f;
+    private float backBound = -2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * -1 * Time.deltaTime * speed);
-    }
+        {
+            transform.Translate(Vector3.left * -1 * Time.deltaTime * speed);
+        }
+
+        if (transform.position.y < backBound && gameObject.CompareTag("Obstacle"))
+
+        {
+            Destroy(gameObject);
+        }
+    } 
 }
