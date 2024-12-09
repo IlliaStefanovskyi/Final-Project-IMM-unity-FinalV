fe1f13f492dcf4fa862064470bb000a23de819b9
Ruben <b00166481@mytudublin.ie>
2024-12-09 14:16:42 +0000

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float timeRemaning;
+    public GameManager gamemanager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRemaning > 0) 
+        {
+            timeRemaning -= Time.deltaTime;
+        }
+        else if (timeRemaning < 0) 
+        {
+            timeRemaning = 0;
+            gamemanager.gameOver();
+            Time.timeScale = 0;
+            timerText.color = Color.red; 
+        }
+        int minutes = Mathf. FloorToInt(timeRemaning / 60); 
+        int seconds = Mathf. FloorToInt(timeRemaning % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+}
